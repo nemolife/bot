7ab15564580d172371be4888f5c33fde6c0d4953
CoopeR <nemolife@gmail.com>
2016-02-27 02:31:07 +0300

    first commit

---
+++ cr4er30.php
@@ -0,0 +1,60 @@
+// <?php 
+
+// ini_set('error_reporting', 'E_ALL');
+
+// $BotToken = "185754253:AAH8VSi0BzesSA7NTcG6gd7qXYIAfT4ubXQ"; #"126421916:AAGvNptpXCJJvRgYBmQayE7JW-Qcl2Wa4t8";
+// $WebSite = "https://api.telegram.org/bot".$BotToken; //."/getUpdates";
+
+// #echo $WebSite;
+// $Update = file_get_contents('php://input'); //'php://input'
+// var_dump($Update);
+
+// // $Update = json_decode($Update, TRUE);
+
+// //  $ChatID = $Update[result][0][message][chat][id];
+// //  $Date = date("d-m-Y H:m:s", $Update[result][0][message][date]);
+// //  $MText = $Update[result][0][message][text];
+ 
+// //  echo $ChatID;
+// //  echo "<br />";
+// //  echo $Date;
+// //  echo "<br />";
+// //  echo $MText;
+
+// // echo "<pre>";
+// // print_r($Update);
+// // echo "</pre>";
+
+// #print_r($Update);
+
+// # $ChatID = $Update['username'];
+// # $Message = $Update["message"]["text"];
+// #echo $ChatID;
+// #echo "<br />";
+// #echo $Message;
+
+// // switch ($Message) {
+// // 	case '/start':
+// // 		 send_message($ChatID, "HEllo PHPBOT");
+// // 		break;
+	
+// // 	default:
+// // 		# code...
+// // 		break;
+// // }
+
+// // echo "hi";
+
+// // function send_message($ChatID, $Message){
+
+// // 	$URL = $GLOBALS[WebSite]."/sendmessage?chat_id="$ChatID."&text=".urlencode($Message);
+// // 	file_get_contents($URL);
+
+// // }
+
+//
+  <?php
+
+  echo "Hello CR4ER30";
+
+?>
