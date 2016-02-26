6a6a0286f087c29ce04d165cc0a23d2208c382d0
nemolife <nemolife@gmail.com>
2016-02-27 02:40:51 +0300

    Update cr4er30.php

--- cr4er30.php
+++ cr4er30.php
@@ -1,19 +1,18 @@
-// <?php 
+<?php 
 
-// ini_set('error_reporting', 'E_ALL');
+ini_set('error_reporting', 'E_ALL');
 
-// $BotToken = "185754253:AAH8VSi0BzesSA7NTcG6gd7qXYIAfT4ubXQ"; #"126421916:AAGvNptpXCJJvRgYBmQayE7JW-Qcl2Wa4t8";
-// $WebSite = "https://api.telegram.org/bot".$BotToken; //."/getUpdates";
+$BotToken = "126421916:AAGvNptpXCJJvRgYBmQayE7JW-Qcl2Wa4t8"; #"126421916:AAGvNptpXCJJvRgYBmQayE7JW-Qcl2Wa4t8";
+$WebSite = "https://api.telegram.org/bot".$BotToken; //."/getUpdates";
 
 // #echo $WebSite;
-// $Update = file_get_contents('php://input'); //'php://input'
+$Update = file_get_contents('php://input'); //'php://input'
 // var_dump($Update);
+$Update = json_decode($Update, TRUE);
 
-// // $Update = json_decode($Update, TRUE);
-
-// //  $ChatID = $Update[result][0][message][chat][id];
-// //  $Date = date("d-m-Y H:m:s", $Update[result][0][message][date]);
-// //  $MText = $Update[result][0][message][text];
+$ChatID = $Update[message][chat][id];
+$Date = date("d-m-Y H:m:s", $Update[message][date]);
+$MText = $Update[message][text];
  
 // //  echo $ChatID;
 // //  echo "<br />";
@@ -27,34 +26,33 @@
 
 // #print_r($Update);
 
-// # $ChatID = $Update['username'];
-// # $Message = $Update["message"]["text"];
+$ChatID = $Update['username'];
+$Message = $Update["message"]["text"];
 // #echo $ChatID;
 // #echo "<br />";
 // #echo $Message;
 
-// // switch ($Message) {
-// // 	case '/start':
-// // 		 send_message($ChatID, "HEllo PHPBOT");
-// // 		break;
+switch ($Message) {
+case '/start':
+	send_message($ChatID, "HEllo PHPBOT");
+ 	break;
 	
-// // 	default:
+ 	default:
 // // 		# code...
-// // 		break;
-// // }
+ 	break;
+}
 
 // // echo "hi";
 
-// // function send_message($ChatID, $Message){
+function send_message($ChatID, $Message){
 
-// // 	$URL = $GLOBALS[WebSite]."/sendmessage?chat_id="$ChatID."&text=".urlencode($Message);
-// // 	file_get_contents($URL);
+ 	$URL = $GLOBALS[WebSite]."/sendmessage?chat_id="$ChatID."&text=".urlencode($Message);
+ 	file_get_contents($URL);
+ }
 
-// // }
+// //
+//   <?php
 
-//
-  <?php
-
-  echo "Hello CR4ER30";
+//   echo "Hello CR4ER30";
 
 ?>
